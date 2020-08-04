f010e1329f08a4ced49c39e2bd6109c2297f973f
KritikPant <kritik1411@outlook.com>
2020-08-04 17:52:17 +0100

    Update

--- ReactNativeChatbot/App.js
+++ ReactNativeChatbot/App.js
@@ -1,39 +1,47 @@
-import React from 'react';
-import { View } from 'react-native';
-import firebase from 'firebase';
-import { Header, Button, Spinner, Card, CardSection } from './src/components/common';
-import LoginForm from './src/components/LoginForm';
-import ChatUI from './src/ChatUI';
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors')
 
-export default class App extends React.Component {
-  state={ isLoggedIn: null };
+var app = express();
 
-  componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyDli82ORX2XTPzWkcygWbqvpC5oncubGLM',
-      authDomain: 'react-native2611.firebaseapp.com',
-      databaseURL: 'https://react-native2611.firebaseio.com',
-      projectId: 'react-native2611',
-      storageBucket: 'react-native2611.appspot.com',
-      messagingSenderId: '1084501269739'
-    });
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-    firebase.auth().onAuthStateChanged((user) => {
-      (user)?this.setState({ isLoggedIn: true }):this.setState({ isLoggedIn: false });
-    });
-  }
+var api = require('./routes/api/index');
+// var feedback = require('./routes/api/feedback');
 
-  showButtonOrForm() {
-    switch(this.state.isLoggedIn) {
-      case true:
-        return (
-          <ChatUI />
-        );
-      case false:
-        return <LoginForm />;
-      default:
-        return <Spinner />;
-    }
-  }
-  render() {return (this.showButtonOrForm());}
-}
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use('/api/', api);
+// app.use('/api/feedback', feedback);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ ReactNativeChatbot/static_store.js
@@ -0,0 +1,7 @@
+const DEV_HOST_URL = 'https://lem-assistant-chatbot-ui.mybluemix.net';
+const LOCAL_HOST_URL = 'http://localhost:3000';
+
+module.exports = {
+    DEV_HOST_URL,
+    LOCAL_HOST_URL
+};

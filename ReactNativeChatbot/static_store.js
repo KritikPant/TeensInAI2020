27679bb84476f2b85cf4bf78f10f8e1a15dffcf4
KritikPant <kritik1411@outlook.com>
2020-08-05 15:57:21 +0100

    Watson Integrated!

--- ReactNativeChatbot/static_store.js
+++
@@ -1,7 +0,0 @@
-const DEV_HOST_URL = 'https://reactnativechatbotteensinai2020.eu-gb.cf.appdomain.cloud/';
-const LOCAL_HOST_URL = 'http://localhost:3000';
-
-module.exports = {
-    DEV_HOST_URL,
-    LOCAL_HOST_URL
-};

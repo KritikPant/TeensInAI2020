a0c70eca173468c0a5d623c7e85cbc456728cf97
KritikPant <kritik1411@outlook.com>
2020-08-06 09:47:25 +0100

    Watson Integrated

--- AndroidChatbot/app/src/main/java/com/example/vmac/WatBot/ChatAdapter.java
+++ AndroidChatbot/app/src/main/java/com/example/vmac/WatBot/ChatAdapter.java
@@ -1,9 +1,5 @@
 package com.example.vmac.WatBot;
 
-/**
- * Created by VMac on 17/11/16.
- */
-
 import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
